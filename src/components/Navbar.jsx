047824592101e9e8ef09e691d4c8b6c4440b03b5
Aishwarya Nair <clockonthewall1213@gmail.com>
2026-04-28 17:12:15 +0530

    resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import { navLinks, navIcons } from "#constants";
+import useWindowStore from "#store/window";
+
 import dayjs from "dayjs";
 
 const Navbar = () => {
+  const { openWindow } = useWindowStore();
+
   return (
     <nav>
       <div>
@@ -9,8 +13,8 @@ const Navbar = () => {
         <p className="font-bold">Aishwarya's Portfolio</p>
 
         <ul>
-          {navLinks.map(({ id, name }) => (
-            <li key={id}>
+          {navLinks.map(({ id, name, type }) => (
+            <li key={id} onClick={() => openWindow(type)}>
               <p>{name}</p>
             </li>
           ))}
